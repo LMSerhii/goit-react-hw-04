1bf30c154362bb214ad6ed9e0c965efb7d2cb6e2
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-02-02 13:35:04 +0200

    gallery style

--- src/componets/ImageCard/ImageCard.jsx
+++ src/componets/ImageCard/ImageCard.jsx
@@ -1,7 +1,10 @@
+import css from './ImageCard.module.css';
+
 export const ImageCard = ({ small, regular, alt_description, onModal }) => {
   return (
     <div>
       <img
+        className={css.img}
         src={small}
         alt={alt_description}
         loading="lazy"
--- src/componets/ImageGallery/ImageGallery.jsx
+++ src/componets/ImageGallery/ImageGallery.jsx
@@ -4,18 +4,30 @@ import css from './ImageGallery.module.css';
 export const ImageGallery = ({ imageList, onModal }) => {
   return (
     <ul className={css.list}>
-      {imageList.map(({ id, alt_description, urls: { small, regular } }) => {
-        return (
-          <li key={id}>
-            <ImageCard
-              small={small}
-              regular={regular}
-              alt_description={alt_description}
-              onModal={onModal}
-            />
-          </li>
-        );
-      })}
+      {imageList.map(
+        ({
+          id,
+          alt_description,
+          likes,
+          urls: { small, regular },
+          user: { name },
+        }) => {
+          return (
+            <li key={id}>
+              <ImageCard
+                small={small}
+                regular={regular}
+                alt_description={alt_description}
+                onModal={onModal}
+              />
+              <div className={css.shortDesc}>
+                <p>Autor: {name}</p>
+                <p>Likes: {likes}</p>
+              </div>
+            </li>
+          );
+        }
+      )}
     </ul>
   );
 };
